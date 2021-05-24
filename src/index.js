640c6db4921bc05fddde73f5e8e74c73b89bb5c7
hiro <hmaeyama0510@gmail.com>
2021-05-24 09:17:48 +0900

    add li tag into the div tag as a child

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ const onClickAdd = () => {
   //liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  console.log(li);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  console.log(div);
 };
 
 document
