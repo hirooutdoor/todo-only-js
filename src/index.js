4ec458a20ca46345bb9fa2fa2c6c2755d84a9a77
hiro <hmaeyama0510@gmail.com>
2021-05-24 09:11:04 +0900

    create div DOM and add class "list-row"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./styles.css";
 
 const onClickAdd = () => {
+  //get the value of input text and clear up the input area
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  //create div
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
 };
 
 document
